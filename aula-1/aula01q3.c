46f7c2fc47dadd6344f30cbb39fcf0216090387b
Davi Eduardo Costa <ed.miranda@tutanota.com>
2024-03-10 16:10:24 -0300

    q3

--- aula-1/aula01q3.c
+++ aula-1/aula01q3.c
@@ -1,19 +1,5 @@
 #include<stdio.h>
 
-//int f(int n) {
-//    int count = n;
-//    int fat = n;
-//
-//    if (n > 0) {
-//        count--;
-//        while(count != 0) {
-//            fat *= count;
-//            count--;
-//        }
-//        return fat;
-//    }
-//}
-
 int fatorial(int n) {
     int resp = 1;
     for(int i = 1; i <= n; i++) {
